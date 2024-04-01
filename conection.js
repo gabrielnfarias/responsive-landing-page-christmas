855156c781e364ef652ae2c90046e7fed53a3695
Bruno Farias <gabrielnavarrofarias@gmail.com>
2024-03-31 23:53:38 -0300

    landing page lf 2.0

--- conection.js
+++ conection.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
   client: "pg",
   connection: {
@@ -9,15 +8,3 @@ module.exports = {
     database: "tornswyu",
   },
 };
-=======
-module.exports = {
-  client: "pg",
-  connection: {
-    host: "kesavan.db.elephantsql.com",
-    port: 5432,
-    user: "tornswyu",
-    password: "zn3444eC6r5Io1cSqJpCKMsWfqX3STRq",
-    database: "tornswyu",
-  },
-};
->>>>>>> 965961549e8370ba706c173dd56618f15eb4db05
--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-<<<<<<< HEAD
 const express = require("express");
-const knexConfig = require("./conection"); // Certifique-se de que este arquivo contém a configuração correta do Knex
+const knexConfig = require("./conection");
 const knex = require("knex");
+const bodyParser = require("body-parser");
 const cors = require("cors");
+const { log } = require("console");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(
-  cors({
-    origin: "https://lfconsultoria.vercel.app", // Substitua pelo domínio real do seu site na Netlify
-  })
-);
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const db = knex(knexConfig);
 
 app.post("/submit", async (req, res) => {
   console.log(req.body);
-
   const { nome, email, celular, cpf, regiao } = req.body;
   try {
     await db("clientes").insert({ nome, email, celular, cpf, regiao });
@@ -29,34 +28,10 @@ app.post("/submit", async (req, res) => {
       .send("Ocorreu um erro ao inserir os dados no banco de dados.");
   }
 });
-
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
-});
-=======
-const express = require("express");
-const knexConfig = require("./conection"); // Certifique-se de que este arquivo contém a configuração correta do Knex
-const knex = require("knex");
-
-const app = express();
-
-app.use(express.json());
-const db = knex(knexConfig);
-
-app.post("/submit", async (req, res) => {
-  const { nomeCompleto, email, celular, cpf, regiao } = req.body;
-  try {
-    await db("clientes").insert({ nomeCompleto, email, celular, cpf, regiao });
-    res.json({ message: "Dados enviados!" });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send("Ocorreu um erro ao inserir os dados no banco de dados.");
-  }
+app.get("/submit", async (req, res) => {
+  res.send("tudo kegal com o get");
 });
 
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
->>>>>>> 965961549e8370ba706c173dd56618f15eb4db05
