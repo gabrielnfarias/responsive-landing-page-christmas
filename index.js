4da725073d05034260e439ae1a8d28856ef28fab
Bruno Farias <gabrielnavarrofarias@gmail.com>
2024-04-01 21:14:08 -0300

    landining test 1.0

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const knexConfig = require("./conection");
 const knex = require("knex");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { log } = require("console");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = knex(knexConfig);
 
@@ -27,9 +23,6 @@ app.post("/submit", async (req, res) => {
       .status(500)
       .send("Ocorreu um erro ao inserir os dados no banco de dados.");
   }
-});
-app.get("/submit", async (req, res) => {
-  res.send("tudo kegal com o get");
 });
 
 app.listen(3000, () => {
