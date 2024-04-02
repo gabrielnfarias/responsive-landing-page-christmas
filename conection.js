70b5f79e195fa1c912cec41b092b2da52c6a4f07
Gabriel Navarro Farias <105251567+gabrielnfarias@users.noreply.github.com>
2024-04-01 23:58:29 -0300

    Backend-exito
    backend-exito 1.0

---
+++ conection.js
@@ -0,0 +1,10 @@
+module.exports = {
+  client: "pg",
+  connection: {
+    host: "kesavan.db.elephantsql.com",
+    port: 5432,
+    user: "tornswyu",
+    password: "zn3444eC6r5Io1cSqJpCKMsWfqX3STRq",
+    database: "tornswyu",
+  },
+};
